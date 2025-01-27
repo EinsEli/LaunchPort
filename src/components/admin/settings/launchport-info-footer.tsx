2c5b098d8514c24305453c2e96ce4f956a34b9d7
EinsEli <57264430+EinsEli@users.noreply.github.com>
2025-01-27 17:38:51 +0100

    Display shortened commit SHA instead of full hash

--- src/components/admin/settings/launchport-info-footer.tsx
+++ src/components/admin/settings/launchport-info-footer.tsx
@@ -3,6 +3,8 @@ import { Separator } from "@/components/ui/separator";
 import Link from "next/link";
 
 export default function LaunchPortInfoFooter() {
+	const commitSha = process.env.COMMIT_SHA;
+
 	return (
 		<div className="flex flex-col items-center justify-center gap-2 text-center text-sm font-medium text-muted-foreground/25">
 			<Separator className="mb-4 mt-2" />
@@ -14,7 +16,18 @@ export default function LaunchPortInfoFooter() {
 				>
 					Version
 				</Link>{" "}
-				1.0.0 • {process.env.COMMIT_SHA || "Local Development"}
+				{/* {process.env.COMMIT_SHA?.substring(0, 7) || "Local Development"} */}
+				1.0.0 •{" "}
+				{commitSha ? (
+					<Link
+						className="transition-colors hover:text-muted-foreground p-1 bg-muted text-xs font-mono rounded-md"
+						href={`https://github.com/EinsEli/LaunchPort/commit/${commitSha}`}
+					>
+						{commitSha.substring(0, 7)}
+					</Link>
+				) : (
+					"Local Development"
+				)}
 			</p>
 		</div>
 	);
